e2d9d8bd79e5f1e6c40bec65e28c3b8e86b90445
Brendan Chan <brendangkchan@gmail.com>
2018-08-24 13:27:28 -0700

    temp: blacklist not working but dir right?

--- metro-bundler-config-yarn-workspaces/index.js
+++ metro-bundler-config-yarn-workspaces/index.js
@@ -5,19 +5,38 @@ const path = require('path')
 module.exports = function getConfig(from, relPathToModules, options = {}) {
   const workspaces = getWorkspaces(from)
 
+  const blacklistedWorkspaceModules = workspaces.map(
+    workspacePath =>
+      `/${workspacePath.replace(
+        /\//g,
+        '[/\\\\]'
+      )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`
+  );
+
+  // const nav = path.resolve(from, relPathToModules, 'node_modules/react-native-tab-view/node_modules/react-native/.*/');
+  const nav = path.resolve(from, relPathToModules, 'node_modules/react-native-tab-view');
+
+  console.log(nav)
+
+  const extraBlacklistedModules = [
+    `/${nav.replace(
+      /\//g,
+      '[/\\\\]'
+    )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`
+    // nav
+  ];
+
+  const allBlacklistedModules = blacklistedWorkspaceModules.concat(extraBlacklistedModules);
+
+  console.log(allBlacklistedModules)
+
   const config = {
     extraNodeModules: {
       'react-native': path.resolve(from, 'node_modules/react-native'),
     },
     getBlacklistRE() {
       return blacklist(
-        workspaces.map(
-          workspacePath =>
-            `/${workspacePath.replace(
-              /\//g,
-              '[/\\\\]'
-            )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`
-        )
+        allBlacklistedModules
       )
     },
     getProjectRoots() {
